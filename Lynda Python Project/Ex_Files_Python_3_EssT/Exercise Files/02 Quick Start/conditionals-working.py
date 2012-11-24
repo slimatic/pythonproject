4573766e5b16bee964bb08191f87c7cdb7c73c2f
salim <salim.31@gmail.com>
2012-11-23 22:28:14 -0500

    initial commit

---
+++ Lynda Python Project/Ex_Files_Python_3_EssT/Exercise Files/02 Quick Start/conditionals-working.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+a, b = 1, 2
+if a < b:
+    print('a ({}) is less than b ({})'.format(a, b))
+else:
+    print('a ({}) is not less than b ({})'.format(a, b))
+
+print('foo')
+print('foo' if a > b else 'bar')
---
+++ Lynda Python Project/Ex_Files_Python_3_EssT/Exercise Files/02 Quick Start/whileloop-working.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# simple fibonacci series
+# the sum of two elements defines the next set
+a, b, c = 0, 1, 0
+while b < 50:
+    print(b)
+    a, b = b, a + b
+
+print("complete")
+
+
+while c < 50:
+    print (c)
+    c = 1 + c
+
+print("done")
